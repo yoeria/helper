34609f5a62a07be31d57d6d7a0104c9ad04bd7d3
yoeria <yoeri08@hotmail.com>
2021-11-02 01:51:50 +0100

    TODO: find outy why shutdownTimer func is err

--- cmd/windows.go
+++ cmd/windows.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os/exec"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -12,22 +13,28 @@ import (
 var windowsCmd = &cobra.Command{
 	Use:   "windows",
 	Short: "Control Windows OS",
-	Long:  `Options:
+	Long: `Options:
 	Shutdown forcefully using timer (in minutes)`,
-	PreRun: func(cmd *cobra.Command, args []string) {if len(args) == 0 {
-		cmd.Help()
+	PreRun: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Help()
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("windows called")
 		fmt.Println(args)
-		
-		// explanation on indexing of slices in golang 
+
+		// explanation on indexing of slices in golang
 		// https://www.golangprograms.com/how-to-get-first-or-last-element-of-slice-in-golang.html
-		minutes := "0"
-		cmd.Execute(shutdownTimer(minutes))
-	}, 
-	Aliases: []string{"win","w"},
+		defaultminutes := "now"
+		minutes := strings.Join(args, "")
+		if minutes == "" {
+			cmd.Execute(shutdownTimer(defaultminutes))
+		} else {
+			cmd.Execute(shutdownTimer(minutes))
+		}
+	},
+	Aliases: []string{"win", "w"},
 }
 
 func init() {
@@ -40,8 +47,8 @@ func shutdownTimer(minutes string) {
 		exec.Command(`shutdown.exe -s -f -t 0`)
 	}
 
-	secondsConverted, err:= (strconv.Atoi(minutes))
+	secondsConverted, err := (strconv.Atoi(minutes))
 	cobra.CheckErr(err)
 	seconds := secondsConverted * 60
-	exec.Command(`shutdown.exe -s -f -t `+strconv.Itoa(seconds))
+	exec.Command(`shutdown.exe -s -f -t ` + strconv.Itoa(seconds))
 }
